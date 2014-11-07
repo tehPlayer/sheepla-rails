3d310c39d703544cec9038617c39934d8cb2cdb2
Marcin Adamczyk <marcin.adamczyk@subcom.me>
2014-11-07 14:45:30 +0100

    create order

--- lib/sheepla.rb
+++ lib/sheepla.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'net/https'
 require 'uri'
 require 'nokogiri'
+require 'ostruct'
 
 module Sheepla
   class ApiError < StandardError; end
@@ -16,7 +17,7 @@ module Sheepla
     end
 
     def get_metro_stations
-      connection('getMetroStationsRequest')
+      connection('getMetroStations')
       body = Nokogiri::XML::Builder.new do |xml|
         xml.getMetroStationsRequest xmlns: "http://www.sheepla.pl/webapi/1_0" do
           authentication(xml)
@@ -26,17 +27,64 @@ module Sheepla
       request_method(body)
     end
 
-    def order_synchronization(params)
-      connection('createOrderRequest')
+    def create_order(params)
+      raise ApiError.new("Order parameters don't contain all obligatory keys") unless validate_order(params)
+      order = params.clone
+
+      connection('createOrder')
+
       body = Nokogiri::XML::Builder.new do |xml|
-        authentication(xml)
+        xml.createOrderRequest xmlns: "http://www.sheepla.pl/webapi/1_0" do
+          authentication(xml)
+          xml.orders do
+            xml.order do
+              xml.orderValue order['order_value']
+              xml.orderValueCurrency order['order_currency']
+              xml.externalDeliveryTypeId order['external_delivery_type_id']
+              xml.externalDeliveryTypeName order['external_delivery_type_name']
+              xml.externalPaymentTypeId order['external_payment_type_id']
+              xml.externalPaymentTypeName order['external_payment_type_name']
+              xml.externalCarrierId order['external_carrier_id']
+              xml.externalCarrierName order['external_carrier_name']
+              xml.externalCountyId order['external_country_id']
+              xml.externalBuyerId order['external_buyer_id']
+              xml.externalOrderId order['external_order_id']
+              xml.shipmentTemplate order['shipment_template']
+              xml.comments order['comments']
+              xml.createdOn order['created_on']
+              xml.deliveryPrice order['delivery_price']
+              xml.deliveryPriceCurrency order['delivery_price_currency']
+              xml.deliveryAddress do
+                xml.street order['delivery_address']['street']
+                xml.zipCode order['delivery_address']['zip_code']
+                xml.city order['delivery_address']['city']
+                xml.countryAlpha2Code order['delivery_address']['country_alpha2_code']
+                xml.firstName order['delivery_address']['first_name']
+                xml.lastName order['delivery_address']['last_name']
+                xml.companyName order['delivery_address']['company_name']
+                xml.phone order['delivery_address']['phone']
+                xml.email order['delivery_address']['email']
+              end
+              xml.deliveryOptions do
+                xml.cod order['delivery_options'].delete('cod')
+                xml.insurance order['delivery_options'].delete('insurance')
+                order['delivery_options'].each do |delivery_partner, delivery_partner_data|
+                  xml.send(delivery_partner) do
+                    delivery_partner_data.each do |k,v|
+                      xml.send(k, v)
+                    end
+                  end
+                end
+              end if order['delivery_options']
+            end
+          end
+        end
       end
 
       request_method(body)
     end
 
     protected
-
       def authentication(xml)
         xml.authentication do
           xml.apiKey @api_key
@@ -68,6 +116,35 @@ module Sheepla
         end
 
         Hash.from_xml(response.body)
+      end
+
+      def validate_order(params)
+        params['order_value'] &&
+        params['order_currency'] &&
+        params['external_delivery_type_id'] &&
+        params['external_delivery_type_name'] &&
+        params['external_payment_type_id'] &&
+        params['external_payment_type_name'] &&
+        params['external_carrier_id'] &&
+        params['external_carrier_name'] &&
+        params['external_country_id'] &&
+        params['external_buyer_id'] &&
+        params['external_order_id'] &&
+        params['shipment_template'] &&
+        params['comments'] &&
+        params['created_on'] &&
+        params['delivery_price'] &&
+        params['delivery_price_currency'] &&
+        params['delivery_address'] &&
+        params['delivery_address']['street'] &&
+        params['delivery_address']['zip_code'] &&
+        params['delivery_address']['city'] &&
+        params['delivery_address']['country_alpha2_code'] &&
+        params['delivery_address']['first_name'] &&
+        params['delivery_address']['last_name'] &&
+        params['delivery_address']['company_name'] &&
+        params['delivery_address']['phone'] &&
+        params['delivery_address']['email']
       end
   end
 end
